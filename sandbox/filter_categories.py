3b1bbe9751440e64d77ac45452eab3f974f582dd
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2023-03-26 19:13:38 +0200

    moving to other computer

--- sandbox/filter_categories.py
+++ sandbox/filter_categories.py
@@ -1,2 +1,6 @@
 # create a plotly express plot that allows the user to turn on and off different combinations of groups in a dataset
 
+import polars as pl
+from sandbox.preprocess import preprocess_dataset1
+dataset = preprocess_dataset1(print_schema=True)
+
--- sandbox/preprocess.py
+++ sandbox/preprocess.py
@@ -2,8 +2,15 @@ import polars as pl
 from polars.dataframe import DataFrame
 from rich import print
 from pathlib import Path
+from typing import Protocol
+from dataclasses import dataclass
 
 pl.toggle_string_cache(True)
+
+@dataclass
+class Dataset(Protocol):
+    ...
+
 
 DATASET1: Path = 'data/Police_Department_Incident_Reports__Historical_2003_to_May_2018.csv'
 DATASET2: Path = 'data/Police_Department_Incident_Reports__2018_to_Present.csv'
@@ -18,8 +25,6 @@ def preprocess_dataset1(n_rows: int = None, print_schema = False, dataset_path: 
     if print_schema:
         print(dataset.schema)
     dataset = dataset.drop_nulls()
-    
-    
     dataset = dataset.with_columns(
         [
             pl.col('Time').str.strptime(
@@ -31,7 +36,7 @@ def preprocess_dataset1(n_rows: int = None, print_schema = False, dataset_path: 
     )
     # TODO: remove columns not needed and prefixes "DELETE - " 
     return dataset
-ds1 = preprocess_dataset1(DATASET1, print_schema=True)
+#ds1 = preprocess_dataset1(DATASET1, print_schema=True)
 
 def preprocess_dataset2(n_rows: int = None, print_schema = False, dataset_path: Path = DATASET2) -> DataFrame:
     dataset = pl.read_csv(dataset_path, try_parse_dates=True, ignore_errors=True, n_rows=n_rows)
@@ -49,7 +54,7 @@ def preprocess_dataset2(n_rows: int = None, print_schema = False, dataset_path: 
     ])
     # TODO: remove columns not needed and prefixes "DELETE - " 
     return dataset
-ds2 = preprocess_dataset2(n_rows = 10, print_schema = True)
+#ds2 = preprocess_dataset2(n_rows = 10, print_schema = True)
 
 # this function combines the two datasets
 def combined_dataset():
@@ -57,3 +62,6 @@ def combined_dataset():
     ds2 = preprocess_dataset2()
 
     shared_columns = ['ROBBERY','GAMBLING','PROSTITUTION','STOLEN PROPERTY','VANDALISM','LIQUOR LAWS','NON-CRIMINAL','OTHER OFFENSES','DISORDERLY CONDUCT','EMBEZZLEMENT','MISSING PERSON','ARSON','SUSPICIOUS OCC','RECOVERED VEHICLE','FRAUD','BURGLARY','SUICIDE','ASSAULT']
+
+def get_lightweight_dataset():
+    return preprocess_dataset1(n_rows=50).to_pandas()
