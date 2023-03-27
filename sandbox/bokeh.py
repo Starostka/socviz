0a8034a9139d454d6c99af5f9f6720a62e830b19
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2023-03-27 23:38:38 +0200

    updates to bokeh plot and small formatting fixes

--- sandbox/bokeh.py
+++ sandbox/bokeh.py
@@ -13,10 +13,10 @@ from bokeh.models import TabPanel, Tabs
 from bokeh.plotting import figure, show
 from bokeh.io import show
 from bokeh.models import Paragraph, ColumnDataSource, FactorRange
-
-
-dataset = pl.read_csv('data/before_2018.csv')
-cat_count = (dataset.groupby('Category').agg(pl.count()))
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure, show
+from bokeh.io import show
+from bokeh.palettes import Category20
 
 dataset = pl.read_csv('data/before_2018.csv')
 data = (
@@ -26,15 +26,59 @@ data = (
     .sort(pl.col('Hour'))
 )
 data = data.pivot(index = 'Hour', columns = ['PdDistrict'], values = ['normalized'])
+# data.schema
+
+focuscrimes = set(['BAYVIEW', 'INGLESIDE', 'TARAVAL', 'CENTRAL'])
+
+source = ColumnDataSource(data.to_pandas())
+hours = data['Hour'].unique().to_list()
+hours = sorted(hours)
+p = figure(title="Crimes per hour", x_axis_label='Hour of the day', y_axis_label='Percentage Frequency', 
+           x_range = FactorRange(factors=[str(hour) for hour in hours]))
+bar = {}
+for indx, i in enumerate(focuscrimes):
+    bar[i] = p.vbar(x="Hour", top=i, width=0.3, 
+                    source=source, legend_label=i, muted_alpha=0.2, muted=False, color=Category20[14][indx])
+p.legend.title = "Category"
+p.legend.location = "top_left"
+
+p.legend.click_policy = "hide"
+p.legend.label_text_color = "black"
+p.legend.title_text_color = "black"
+show(p)
 
 
+
+
+
+
+df = pd.read_csv('data/before_2018.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df['Year'] = df['Date'].dt.year
+df = df[df.Year >= 2010].reset_index(drop=True)
+df = df[df.Year < 2018].reset_index(drop=True)
+df['Hour'] = pd.DatetimeIndex(df['Time']).hour
+hourly_pattern = df.groupby(by=["Hour", "PdDistrict"]).size().reset_index(name="Occurence")
+hourly_pattern = hourly_pattern.loc[hourly_pattern['PdDistrict'].isin(focuscrimes)].reset_index(drop=True)
+category = hourly_pattern['PdDistrict'].unique().tolist()
+hourly_pattern['Sum'] = hourly_pattern.groupby('PdDistrict')['Occurence'].transform('sum')
+hourly_pattern['Norm'] = hourly_pattern['Occurence'] / hourly_pattern['Sum']
+hourly_pattern = hourly_pattern.pivot_table(index='Hour', columns='PdDistrict', values='Norm')
+hourly_pattern
+
+
+
+
+
+
+src = ColumnDataSource(data.to_pandas())
 p = figure(x_range = FactorRange(factors=data['Hour'].unique()), title="Department district - Performance") 
 
-p1 = figure(width=600, height=300, title="Robbery incidents")
+bar = {}
+for idx, row in enumerate(data.rows(named=True)):
+    print(row)
+    break
 
-bar ={} # to store vbars
-### here we will do a for loop:
-for indx,i in enumerate(focuscrimes):
-    bar[i] = p.vbar(x='name_of_the_column_that_contain_hours',  top=i, source= src, 
-                    ### we will create a vbar for each focuscrime
-                    legend_label=i,  muted_alpha=..., muted = ....) 
+
+    bar[row['']] = p.vbar(x='Hour',  top=i, source=src,
+        legend_label=i,  alpha=0.7) 
