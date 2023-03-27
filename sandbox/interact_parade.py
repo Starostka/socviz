e610f1d30038141ca603be11658d68edc5a09d0a
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2023-03-27 23:09:44 +0200

    updates again

--- sandbox/interact_parade.py
+++ sandbox/interact_parade.py
@@ -15,8 +15,9 @@ from bokeh.models import TabPanel, Tabs
 from bokeh.plotting import figure, show
 from bokeh.io import show
 from bokeh.models import Paragraph
+from bokeh.transform import dodge
 from folium.plugins import HeatMap
-from sandbox.preprocess import preprocess_dataset1
+# from sandbox.preprocess import preprocess_dataset1
 
 # == generate files
 # df_before_2018 = pd.read_csv('data/Police_Department_Incident_Reports__Historical_2003_to_May_2018.csv')
@@ -31,7 +32,7 @@ from sandbox.preprocess import preprocess_dataset1
 # get_XY = list(zip(list(df_robbery_pride["Y"]), list(df_robbery_pride["X"])))
 # dir(df_before_2018)
 
-dataset = pl.read_csv('before_2018.csv')
+dataset = pl.read_csv('data/before_2018.csv')
 
 data = (
     dataset
@@ -46,7 +47,8 @@ par1 = Paragraph(text=f"""
     Mean incident count: {data['count'].mean():3.2f}
     """,
     width=600, height=50)
-tab1 = TabPanel(child=column(par1, p1), title="ROBBERY")
+tab2 = TabPanel(child=p2, title="ROBBERY")
+
 
 data = (
     dataset
@@ -61,6 +63,55 @@ par2 = Paragraph(text=f"""
     Mean incident count: {data['count'].mean():3.2f}
     """,
     width=600, height=50)
-tab2 = TabPanel(child=column(par2, p1), title="WARRANTS")
+show(p2)
 
+tabs = TabPanel(child=column(par2, p1), title="WARRANTS")
+show()
+
+par1 = Paragraph(text=f"""
+    Mean incident count: {data['count'].mean():3.2f}
+    """,
+    width=600, height=50)
+tab1 = TabPanel(child=column(par1, p1), title="ROBBERY")
+
+data = (
+    dataset
+    .filter(pl.col('Category') == 'ROBBERY')
+    .groupby('Year').agg(pl.count())
+).to_pandas()
+p1 = figure(width=600, height=300, title="Robbery incidents")
+p1.xgrid.grid_line_color = None
+p1.xaxis.ticker = sorted(data['Year'].to_list())
+p1.vbar(x=data['Year'], top=data['count'], width=0.9, alpha=0.4)
+par2 = Paragraph(text=f"""
+    Mean incident count: {data['count'].mean():3.2f}
+    """,
+    width=600, height=50)
+tab2 = TabPanel(child=column(par2, p1), title="WARRANTS")
 show(Tabs(tabs=[tab1, tab2]))
+
+
+
+data = (
+    dataset
+    .filter(pl.col('Category').is_in(['ROBBERY', 'WARRANTS']))
+    .filter(pl.col('Year').is_in([2015, 2016, 2017]))
+    .groupby(['Category', 'Year']).agg(pl.count().alias('Count'))
+)
+d1 = data.filter(pl.col('Category') == 'ROBBERY').to_pandas()
+d2 = data.filter(pl.col('Category') == 'WARRANTS').to_pandas()
+
+CATEGORIES = ['ROBBERY', 'WARRANTS']
+p = figure(x_range=CATEGORIES, y_range=(0, 7000), title="Incident count per year",
+           height=350, toolbar_location=None, tools="")
+p.vbar(x=dodge('Category', 0.0, range=p.x_range), top='Count', source = d1,
+       width=0.2, color="#c9d9d3", legend_label="2015")
+
+p.vbar(x=dodge('Category',  0.0,  range=p.x_range), top='Count', source = d2,
+       width=0.2, color="#718dbf", legend_label="2016")
+
+p.x_range.range_padding = 0.1
+p.xgrid.grid_line_color = None
+p.legend.location = "top_left"
+p.legend.orientation = "horizontal"
+show(p)
